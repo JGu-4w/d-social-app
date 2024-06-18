81618bcd2bde191e78cab2496ab22cf3ac5dfee9
Joe Gu <joegu.4work@gmail.com>
2024-06-18 15:49:42 +0800

    feat: add linea-sepolia chain config

---
+++ decen-social-app/src/app/chain.ts
@@ -0,0 +1,28 @@
+import { defineChain } from "thirdweb";
+
+const chain = defineChain({
+  id: 59141,
+  explorers: [
+    {
+      name: "Etherscan",
+      url: "https://sepolia.lineascan.build",
+      standard: "EIP3091",
+    },
+  ],
+  name: "Linea Sepolia",
+  nativeCurrency: {
+    name: "Linea Ether",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  networkId: 59141,
+  rpc: ["https://rpc.sepolia.linea.build"],
+  shortName: "linea-sepolia",
+  chain: "ETH",
+  chainId: 59141,
+  testnet: true,
+  slug: "linea-sepolia",
+  title: "Linea Sepolia Testnet",
+});
+
+export default chain;
